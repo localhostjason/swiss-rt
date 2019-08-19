f71653222db696c745206e280abe440c6a2f520b
zhangjie <zhangjie@tcxa.com.cn>
2019-08-19 16:39:49 +0800

    change static app

--- api/admin/static_file.py
+++ api/admin/static_file.py
@@ -5,3 +5,13 @@ from .. import app
 @app.route('/')
 def index():
     return redirect('/app/index.html')
+
+
+@app.route('/app')
+def app_():
+    return redirect('/app/index.html')
+
+
+@app.route('/admin')
+def admin_():
+    return redirect('/admin/index.html')
